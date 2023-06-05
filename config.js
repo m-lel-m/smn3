52d4980a8a09e5495fe24150e64d18f33f50b7ee
m-lel-m <134222393+m-lel-m@users.noreply.github.com>
2023-06-05 08:07:29 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // Set up your project here
-const author = 'keneanung';  // author name on GitHub, might also be an org
-const projectName = 'iron-realms-nexus-script-template'; // project name on GitHub
+const author = 'm-lel-m';  // author name on GitHub, might also be an org
+const projectName = 'smn3'; // project name on GitHub
 const libraryName = 'Greeter'; // name of the resulting library. Used by webpack to create a global name
 const npmAuthorName = 'keneanung'; // name of the author for NPM
 const npmPackageName = 'nexus_script_template'; // Name of the NPM package that should be published
 const projectDescription = 'Template for nexus scripts using typescript'; // Project description in the NPM package
 const authorEmail = 'keneanung@gmail.com' // author email address
-const docuTitle = 'Iron Realms Nexus Script Template'; // Title of the documentation website
-const docuTagline = 'Making Nexus scripts easier'; // Used for documentation website
+const docuTitle = ' '; // Title of the documentation website
+const docuTagline = ' '; // Used for documentation website
 
 
 // don't modify the exports below (except to add new fields)
